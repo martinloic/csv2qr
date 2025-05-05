e10814632093792f68af4f2bcc8c7d2fdf1abab9
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-05-05 16:27:14 +0200

    fix: Correct warning message for missing "code" column in CSV

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ fs.createReadStream(csvFilePath)
     if (row.code && typeof row.code === 'string') {
       qrCodes.push(row.code); // Remplacez 'url' par le nom de la colonne appropriée
     } else {
-      console.warn('Ligne CSV invalide ou colonne "url" manquante:', row);
+      console.warn('Ligne CSV invalide ou colonne "code" manquante:', row);
     }
   })
   .on('end', () => {
@@ -28,61 +28,59 @@ fs.createReadStream(csvFilePath)
 
 // Générer un PDF avec les QR codes
 function generatePDF(qrCodes) {
-    const doc = new PDFDocument({
-      size: [283.46, 425.2], // Dimensions en points (10 cm x 15 cm)
-      margin: 10
+  const doc = new PDFDocument({
+    size: [283.46, 425.2], // Dimensions en points (10 cm x 15 cm)
+    margin: 10
+  });
+  const pdfFilePath = path.join(__dirname, 'output.pdf'); // Chemin du fichier PDF à la racine
+  const writeStream = fs.createWriteStream(pdfFilePath);
+  doc.pipe(writeStream);
+
+  let qrCodesProcessed = 0;
+  let yPosition = 10;
+  const qrCodeSize = 100;
+  const maxQRPerPage = 4;
+
+  qrCodes.forEach((text, index) => {
+    QRCode.toDataURL(text, (err, url) => {
+      if (err) {
+        console.error('Erreur lors de la génération du QR code:', err);
+        return;
+      }
+
+      // Vérifier si on doit ajouter une nouvelle page
+      if (index > 0 && index % maxQRPerPage === 0) {
+        doc.addPage({
+          size: [283.46, 425.2], // Dimensions en points (10 cm x 15 cm)
+          margin: 10
+        });
+        yPosition = 10;
+      }
+
+      // Ajouter le QR code et le texte
+      doc.image(url, 10, yPosition, { width: qrCodeSize });
+      doc.font('Helvetica-Bold');
+      doc.fontSize(21).text(text, 130, yPosition + 42, { width: 150, align: 'left' });
+      doc.font('Helvetica');
+
+      // Ajouter une ligne en pointillés sous le QR code et le texte
+      doc.moveTo(10, yPosition + qrCodeSize)
+         .lineTo(273.46, yPosition + qrCodeSize)
+         .dash(5, { space: 5 })
+         .stroke();
+
+      // Mettre à jour la position y pour la ligne suivante
+      yPosition += qrCodeSize; // 50 pour inclure la hauteur du texte, la ligne en pointillés et une marge
+
+      qrCodesProcessed++;
+
+      if (qrCodesProcessed === qrCodes.length) {
+        doc.end();
+        writeStream.on('finish', () => {
+          console.log('PDF généré avec succès:', pdfFilePath);
+          // Vous pouvez ajouter ici le code pour imprimer le PDF si nécessaire
+        });
+      }
     });
-    const pdfFilePath = path.join(__dirname, 'output.pdf'); // Chemin du fichier PDF à la racine
-    const writeStream = fs.createWriteStream(pdfFilePath);
-    doc.pipe(writeStream);
-  
-    let qrCodesProcessed = 0;
-    let yPosition = 10;
-  
-  
-    qrCodes.forEach((text, index) => {
-      QRCode.toDataURL(text, (err, url) => {
-        if (err) {
-          console.error('Erreur lors de la génération du QR code:', err);
-          return;
-        }
-  
-        // Vérifier si on doit ajouter une nouvelle page
-        if (yPosition + 100 > doc.page.height - 10) {
-          doc.addPage({
-            size: [283.46, 425.2], // Dimensions en points (10 cm x 15 cm)
-            margin: 10
-          });
-          yPosition = 20;
-        }
-  
-        doc.image(url, 20, yPosition, { width: 100 });
-
-        // Définir la police en gras
-        doc.font('Helvetica-Bold');
-        doc.fontSize(15).text(`${text}`, 130, yPosition + 20, { width: 120, align: 'left' });
-
-        // Réinitialiser la police à la police par défaut
-        doc.font('Helvetica');
-
-        // doc.fontSize(15).text(text, 130, yPosition + 20, { width: 120, align: 'left' });
-
-        doc.moveTo(10, yPosition)
-           .lineTo(273.46, yPosition)
-           .dash(5, { space: 5 })
-           .stroke();
-        // yPosition += 10;
-        yPosition += 100;
-        qrCodesProcessed++;
-        
-        if (qrCodesProcessed === qrCodes.length) {
-          doc.end();
-          writeStream.on('finish', () => {
-            console.log('PDF généré avec succès:', pdfFilePath);
-            // Vous pouvez ajouter ici le code pour imprimer le PDF si nécessaire
-          });
-        }
-      });
-    });
-  }
-  
+  });
+}
